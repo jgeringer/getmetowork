24227679526ebb5dc54345e8c238964c5c11fc73
Joseph Geringer <joegeringer@gmail.com>
2018-03-13 16:22:59 -0500

    Fixed api call

--- getmetowork/src/components/Bus.vue
+++ getmetowork/src/components/Bus.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         Next Bus: 
-        <div v-for="bus in nextBus.prd">
-            {{bus.prdctdn}}
+        <div v-for="bus in nextBus">
+            <p>Time Left: {{bus.prdctdn}}</p>
         </div>
         {{transitError}}    
     </div>
@@ -10,6 +10,7 @@
 
 <script>
 import axios from 'axios'
+import { BUS_KEY } from '@/constants'
 
 export default {
     name: "Bus",
@@ -25,10 +26,13 @@ export default {
     methods: {
         getBusses: function(){
             console.log('getBusses')
+            console.log('BUS_KEY', BUS_KEY)
+            return; ////REMOVE WHEN READY
+
             axios.get("https://can-cors.herokuapp.com/http://www.ctabustracker.com/bustime/api/v2/getpredictions?key=5kQ32nKLH7YSWPBxsw4kX7q7h&rt=80&stpid=14550&format=json") //90(harlem), 7861(belmont), 1813(montrose) wb: rt-78(stpid: 14813)
             .then((response) => {
-                console.warn('response: '+ response)
-                this.nextBus = response["bustime-response"];
+                console.log(response.data["bustime-response"])
+                this.nextBus = response.data["bustime-response"].prd;
             }, (error) => {
                 this.transitError = error;
                 console.log('myerror: ', this.transitError)
---
+++ getmetowork/src/constants.js
@@ -0,0 +1,4 @@
+export const BUS_KEY = "5kQ32nKLH7YSWPBxsw4kX7q7h"
+
+
+
